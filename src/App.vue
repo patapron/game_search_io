add5992dabf2ef5dac6f09f21fe9ce35987a3a1d
patapron <pronick@gmail.com>
2019-01-15 02:30:18 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+
+
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>              
+      <v-btn color="success">Success</v-btn>
+      <v-icon>home</v-icon>                  
+      <div class="text-xs-center">
+        <v-rating v-model="rating"></v-rating>
+      </div>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      rating: 4
+    }
+  }
+}
+</script>
